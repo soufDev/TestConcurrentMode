b73dfd16bc71ca84ab14c0f8eb2fe796a3a377dc
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2019-11-04 13:30:38 +0100

    :sparkles: move to concurrent mode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeContext } from 'styled-components';
+import { TabProvider } from './components/store';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import theme from './theme';
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Container = (
+    <ThemeContext.Provider value={theme}>
+        <TabProvider>
+            <App />
+        </TabProvider>
+    </ThemeContext.Provider>
+)
+ReactDOM.createRoot(document.getElementById('root')).render(Container);
