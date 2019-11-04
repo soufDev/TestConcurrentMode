7b53e0ed256260f597900631f6d24ffd86629f03
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2019-11-04 13:30:18 +0100

    :sparkles: make the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Tabs from './components/Tabs';
+import TabContent from './components/TabContent';
+import { useTabReducer } from './components/store';
+import Spinner from './components/Spinner';
+import { Container, TabContainer } from './App.styled';
+
+const Summary = React.lazy(() => import('./components/Summary'));
+const Stats = React.lazy(() => import('./components/Stats'));
+const Reviews = React.lazy(() => import('./components/Reviews'));
 
 function App() {
+  const { state: tabState } = useTabReducer();
+  const { visibleTab } = tabState;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Suspense fallback={<Spinner />}>
+      <Container>
+        <Tabs />
+        <TabContainer>
+          <TabContent isVisible={visibleTab === 1}>
+            <Summary />
+          </TabContent>
+          <TabContent isVisible={visibleTab === 2}>
+            <Stats />
+          </TabContent>
+          <TabContent isVisible={visibleTab === 3}>
+            <Reviews />
+          </TabContent>
+        </TabContainer>
+      </Container>
+    </React.Suspense>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { TabProvider } from './components/store';
+
+const Tree = (
+  <TabProvider>
+    <App />
+  </TabProvider>
+)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(Tree, div);
   ReactDOM.unmountComponentAtNode(div);
 });
